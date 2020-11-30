d5a23ca0fed744850a3b7c3f1357808c0cc7b0fa
unknown <mrl.remi@gmail.com>
2020-11-30 10:04:33 +0100

    added randomMeal

--- script.js
+++ script.js
@@ -2,26 +2,69 @@ const searchInput = document.getElementById('searchInput');
 const results = document.getElementById('results');
 const randomMeal = document.getElementById('randomMeal');
 
-let search = ' ';
+let urlSearch = '';
 
-const fetchSearch = async () => {
-	meals = fetch(
-		`https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`)
+const fetchSearch = async (url) => {
+	meals = await fetch(
+		`https://www.themealdb.com/api/json/v1/1/${url}`)
 		.then(res => res.json())
 		.then(res => res.meals)
-	console.log(meals);
-}
+};
 
 const searchDisplay = async () => {
-	await fetchSearch();
-	if (meals == null ) {
-		results.innerHTML= ' <span class="noResult"> Aucun Résultat </span>';
+	await fetchSearch(urlSearch);
+
+	if (meals == null) {
+		results.innerHTML = `<span class="noResult">No results</span>`
 	}
+
+	results.innerHTML = (
+
+		meals.map(meal => (
+
+			`
+      <div class="searchContainer">
+        <h2>${meal.strMeal}</h2>
+        <div class="infos">
+          <div>origin : ${meal.strArea}</div>
+          <div>category : ${meal.strCategory}</div>
+        </div>
+        <img src='${meal.strMealThumb}' /></br>
+        <a href="${meal.strYoutube}" target="_blank"><i class="fab fa-youtube"></i></a>
+      </div>
+      `
+		)).join('')
+	);
 };
 
 searchInput.addEventListener('input', (e) => {
-	search = e.target.value
+	urlSearch = `search.php?s=${e.target.value}`;
 	searchDisplay();
-})
+});
 
-festchSearch();
+// repas aléatoire
+
+const randomMealDisplay = async () => {
+	await fetchSearch('random.php');
+
+	results.innerHTML = (
+
+		meals.map(meal => (
+			`
+        <div class="randomContainer">
+          <h2>${meal.strMeal}</h2>
+          <div class="infos">
+            <div>origin : ${meal.strArea}</div>
+            <div>catégory : ${meal.strCategory}</div>
+          </div>
+          <img src='${meal.strMealThumb}' />
+          <p>${meal.strInstructions}</p>
+          <a href="${meal.strYoutube}" target="_blank"><i class="fab fa-youtube"></i></a>
+        </div>
+      `
+		))
+	);
+};
+
+randomMeal.addEventListener('click', randomMealDisplay)
+randomMealDisplay();
