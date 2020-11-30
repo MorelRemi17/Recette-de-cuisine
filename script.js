ad1f5c1a86c660fc5b05a36608ef1f0fb63c3c9c
unknown <mrl.remi@gmail.com>
2020-11-30 09:34:26 +0100

    setting search bar

--- script.js
+++ script.js
@@ -2,11 +2,26 @@ const searchInput = document.getElementById('searchInput');
 const results = document.getElementById('results');
 const randomMeal = document.getElementById('randomMeal');
 
-const festchSearch = async ( ) => {
-    meals=fetch(
-        `https://www.themealdb.com/api/json/v1/1/search.php?s=tomato`
-    ).then( res => res.json ())
-    .then(res => console.log(res))
+let search = ' ';
+
+const fetchSearch = async () => {
+	meals = fetch(
+		`https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`)
+		.then(res => res.json())
+		.then(res => res.meals)
+	console.log(meals);
 }
+
+const searchDisplay = async () => {
+	await fetchSearch();
+	if (meals == null ) {
+		results.innerHTML= ' <span class="noResult"> Aucun Résultat </span>';
+	}
+};
+
+searchInput.addEventListener('input', (e) => {
+	search = e.target.value
+	searchDisplay();
+})
 
 festchSearch();
